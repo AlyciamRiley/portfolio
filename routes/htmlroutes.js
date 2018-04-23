ce43195581c6d16db8037343c3322902519c8c31
unknown <alyciarileym@gmail.com>
2018-04-22 19:57:18 -0500

    added routes

---
+++ routes/htmlroutes.js
@@ -0,0 +1,30 @@
+var path = require("path");
+
+module.exports = function(app){
+
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../splash.html"));
+    });
+
+app.get("/main", function(req, res){
+    res.sendFile(path.join(__dirname, "../public/main.html"));
+});
+
+app.get("/about", function(req, res){
+    res.sendFile(path.join(__dirname, "../public/about.html"));
+});
+
+app.get("/projects", function(req, res){
+    res.sendFile(path.join(__dirname, "../public/projects.html"));
+    
+});
+
+app.get("/resume", function(req, res){
+    res.sendFile(path.join(__dirname, "../public/resume.html"));
+});
+
+
+
+
+
+};
---
+++ server.js
@@ -0,0 +1,33 @@
+//=========================================================================
+//DEPENDENCIES
+//=========================================================================
+
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+
+//=========================================================================
+//EXPRESS CONFIGURATION
+//=========================================================================
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public')); 
+
+//=========================================================================
+//ROUTER
+//=========================================================================
+
+require(path.join(__dirname,"./routes/htmlRoutes"))(app);
+
+//=========================================================================
+//LISTENER
+//=========================================================================
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
